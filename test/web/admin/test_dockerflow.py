e96d7f5e6d78c65c8fc596f52ecf61a05aaa3ce9
Ben Hearsum <bhearsum@mozilla.com>
2018-08-29 19:32:40 -0400

    Add remaining dockerflow tests.

--- test/web/admin/test_dockerflow.py
+++ test/web/admin/test_dockerflow.py
@@ -8,3 +8,29 @@ def test_version(balrogadmin):
 }
 """
     assert ret.get_data(as_text=True) == expected
+
+def testHeartbeat(mocker, balrogadmin):
+    cr = mocker.patch("auslib.global_state.dbo.dockerflow.incrementWatchdogValue")
+    cr.side_effect = (1, 2, 3)
+    for i in range(1, 3):
+        ret = balrogadmin.get("/__heartbeat__")
+        assert ret.status_code == 200
+        assert cr.call_count == i
+        assert ret.headers["Cache-Control"] == "public, max-age=60"
+        assert int(ret.get_data(as_text=True)) == i
+
+def testHeartbeatWithException(mocker, balrogadmin):
+    cr = mocker.patch("auslib.global_state.dbo.dockerflow.incrementWatchdogValue")
+    cr.side_effect = Exception("kabom!")
+    # Because there's no web server between us and the endpoint, we receive
+    # the Exception directly instead of a 500 error
+    ret = balrogadmin.get("/__heartbeat__")
+    assert ret.status_code == 502
+    assert cr.call_count == 1
+    assert ret.get_data(as_text=True) == "Can't connect to the database."
+    assert ret.headers["Cache-Control"] == "public, max-age=60"
+
+def testLbHeartbeat(mocker, balrogadmin):
+    ret = balrogadmin.get("/__lbheartbeat__")
+    assert ret.status_code == 200
+    assert ret.headers["Cache-Control"] == "no-cache"
